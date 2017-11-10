bd6437edf5e214d0fd7e304bf15cd75688b4240f
vijay <vijayanbu001@gmail.com>
2017-11-10 07:25:00 +0530

    image slider added

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { Image, View, Text, StyleSheet } from "react-native";
+import {
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  ViewPagerAndroid
+} from "react-native";
+import {Button,Container,Content} from "native-base";
 import { LoginScreen } from "./LoginScreen";
-import styles  from "./../styles/AppStyle";
+import styles from "./../styles/AppStyle";
+// import ImageSlider from "react-native-image-slider";
 // import { StackNavigator,DrawerNavigator } from "react-navigation";
 
 class SplashScreen extends Component {
@@ -10,15 +18,71 @@ class SplashScreen extends Component {
   }
   componentWillMount() {
     var navigator = this.props.navigation;
-    setTimeout(() => {
+    /*   setTimeout(() => {
       navigator.navigate("MainNavigator");
-    }, 1000);
+    }, 10000); */
   }
-  
+
   render() {
     return (
-      <View style={styles.splashImage}>
-        <Image source={require("./../images/ic_app_notification.png")} />
+      <View style={{ flex: 1,justifyContent:'space-between' , backgroundColor: "#ccc" }}>
+        <View style={{flex:1,justifyContent:'center'}}>
+          <Image source={require(".././images/ic_app_notification.png")} />
+        </View>
+        <View style={{flex:4}}> 
+          <ViewPagerAndroid
+            style={{flex:1 , justifyContent: "center" }}
+            initialPage={0}
+          >
+            <View style={{ flex:1, justifyContent: "center" }}>
+              <Text
+                style={{
+                  alignSelf:'center',
+                  fontSize: 35,
+                  color: "#800"
+                }}
+              >
+                12,321 players & 113 teams
+              </Text>
+            </View>
+            <View style={{ flex:1, justifyContent: "center" }}>
+            <Text
+              style={{
+                alignSelf:'center',
+                fontSize: 35,
+                color: "#800"
+              }}
+              >
+                312 tournaments
+              </Text>
+            </View>
+            <View style={{ flex:1, justifyContent: "center" }}>
+            <Text
+              style={{
+                alignSelf:'center',
+                fontSize: 35,
+                color: "#800"
+              }}
+              >
+                45+ matches
+              </Text>
+            </View>
+            <View style={{ flex:1, justifyContent: "center" }}>
+            <Text
+              style={{
+                alignSelf:'center',
+                fontSize: 35,
+                color: "#800"
+              }}
+              >
+                16 teams
+              </Text>
+            </View>
+          </ViewPagerAndroid>
+        </View>
+        <View style={{flex:1,justifyContent:'center'}}>
+          <Button rounded style={{alignSelf:'center',backgroundColor:'#800'}}><Text style={{textAlign:'center',color:"white",width:'50%'}}>Become an Ontr'ite</Text></Button>
+        </View>
       </View>
     );
   }
